1e7a49d18ac280e8345692c823a1aaf07eda965f
natasjakorten <n.korten@live.nl>
2023-07-28 00:38:06 +0200

    achtste commit

--- src/components/context/AuthContext.js
+++ src/components/context/AuthContext.js
@@ -1,7 +1,7 @@
 import { React, createContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import jwtDecode from "jwt-decode"
-import axios from axios
+import axios from "axios"
 
 export const AuthContext = createContext({});
 
@@ -9,39 +9,86 @@ function AuthContextProvider({ children }) {
     const [isAuth, setIsAuth] = useState
     isAuthenticated: false,
         user: null,
-        status:'pending'
+        status:'pending',
 });
 
  useEffect(effect() =>{
     const token = localStorage.getItem(key:'token');
-    const decodeToken = jwtDecode(token);
-    console.log(decodedToken);
     if (token) {
-        void fetchUserData(decodedtoken, token);
+        const decodeToken = jwtDecode(token);
+        console.log(decodedToken);
+        void fetchUserData(decodedToken, token);
     } else {
+        setIsAuth( value:{
+        ...isAuth,
+                status:'done',
+        });
     }
 }, deps: []);
+
     const navigate = useNavigate();
 
+    async function fetchUserData(decodeToken, token) {
+        try {
+            const response = await axios.get(url
+        :
+            'http://localhost:3000/600/users/${decodedToken.sub}', config
+        :
+            {
+                headers: {
+                    "content-Type"
+                :
+                    "application/json",
+                        Authorization
+                :
+                    'Bearer ${token}',
+                }
+            }
+        )
+            ;
+            setIsAuth(value
+        :
+            {
+                isAuthenticated: true,
+                    user
+            :
+                {
+                    id: response.data.id, username
+                :
+                    response.data.username,
+                        email
+                :
+                    response.data.email,
+                }
+            ,
+                status:'done',
+            }
+        )
+            ;
+            console.error(e);
+        }
+    }
+        } catch(e) {
+       setIsAuth( value:{
+           ...isAuth,
+                        status:'done',
+                });
+
+            console.error(e);
+        }
+    }
     function login(token) {
         console.log(token);
-        localStorage.setItem('token,token')
+        localStorage.setItem('token', token);
         console.log('Gebruiker is ingelogd!');
-        setIsAuth( {
-            isAuthenticated: true,
-                user: {
-                username: 'Piet',
-                email: 'Piet@Novi.nl',
-                wachtwoord: '123456',
-            }
-        });
+        void fetchUserData(decodedToken, token);
         navigate('/profile');
     }
 
     function logout() {
         console.log('Gebruiker is uitgelogd!');
         setIsAuth( value: {
-            isAuthenticated: false
+            isAuthenticated: false,
             user: null,
         });
         navigate('/');
@@ -55,7 +102,7 @@ function AuthContextProvider({ children }) {
 
     return (
         <AuthContext.Provider value={contextData}>
-            {children}
+            {isAuth.status=== 'done'? children: <p>Loading...</p>}
         </AuthContext.Provider>
     )
 }
